249b44681be65f7f23b29842999e3f1e25bef3d2
Marvin Kome <marvinkome@gmail.com>
2019-10-25 17:15:38 +0100

    [#25] - design all posts header

--- client/src/pages/admin/posts/allPosts/index.js
+++ client/src/pages/admin/posts/allPosts/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import Layout from 'components/layout';
+import { MdSearch } from 'react-icons/md';
+
+import './all_posts.scss';
 
 export default class AllPosts extends React.Component {
     render() {
         return (
             <Layout>
                 <div className="db_all_posts">
-                    <h1 className="page__name">All Posts</h1>
+                    <div className="page-header">
+                        <h2 className="page__name">All Posts</h2>
+
+                        <div className="nav--lists">
+                            <select className="select">
+                                <option>Select category</option>
+                                <option>Uncategorized</option>
+                            </select>
+
+                            <form className="nav--form">
+                                {' '}
+                                <MdSearch className="search--icon" />
+                                <input
+                                    className="nav--search"
+                                    type="text"
+                                    placeholder="Search posts"
+                                />
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </Layout>
         );
